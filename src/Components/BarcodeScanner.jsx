a4d33a18482dba9ef984db6ec55b9f548c4850bb
Vickrant <vickrantselvendran@gmail.com>
2025-03-28 13:11:00 +0530

    Autofocus fix v2.5

--- src/Components/BarcodeScanner.jsx
+++ src/Components/BarcodeScanner.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { BarcodeScanner as BarcodeScannerComponent } from "react-barcode-scanner";
 
 const BarcodeScanner = () => {
